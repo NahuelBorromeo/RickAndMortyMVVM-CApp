a537a40af1b6642378234fb90d0a71875c3c239d
Nahuel Borromeo <nahuelborromeo@gmail.com>
2024-09-29 18:05:58 -0500

    Initial Commit

---
+++ RickAndMortyMVVM-CApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMortyMVVM-CApp
+//
+//  Created by Nahuel Borromeo Da costa on 29/09/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
